462a7ef70c3829cc9efb7a8af16a0a9aaec43e63
macnack <krupka.maciej@icloud.com>
2022-01-24 17:23:14 +0100

    upp codu

--- Przeszukiwanie i planowanie ścieżki/uczenie maszynowe/main.py
+++ Przeszukiwanie i planowanie ścieżki/uczenie maszynowe/main.py
@@ -2,13 +2,13 @@
 
 """code template"""
 
+import os
 import random
 import numpy as np
 import cv2
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import pandas
-import os
 
 # translation of 43 classes to 3 classes:
 # 0 - prohibitory
@@ -70,7 +70,6 @@ def load_data(path, filename):
     entry_list = pandas.read_csv(os.path.join(path, filename))
 
     data = []
-    class_to_num = {}
     for idx, entry in entry_list.iterrows():
         class_id = class_id_to_new_class_id[entry['ClassId']]
         image_path = entry['Path']
@@ -79,14 +78,8 @@ def load_data(path, filename):
             image = cv2.imread(os.path.join(path, image_path))
             data.append({'image': image, 'label': class_id})
 
-            if class_id not in class_to_num:
-                class_to_num[class_id] = 0
-            class_to_num[class_id] += 1
-    class_to_num = dict(sorted(class_to_num.items(), key=lambda item: item[0]))
-    print(class_to_num)
-
     return data
-
+    
 
 def learn_bovw(data):
     """
@@ -226,7 +219,6 @@ def evaluate(data):
     # this function does not return anything
     return
 
-
 def display(data):
     """
     Displays samples of correct and incorrect classification.
@@ -260,13 +252,13 @@ def display(data):
     corr = dict(sorted(corr.items(), key=lambda item: item[0]))
     corr_disp = {}
     for key, samples in corr.items():
-        idxs = random.sample(samples, grid_size)
+        idxs = random.sample(samples, min(grid_size, len(samples)))
         corr_disp[key] = [data[idx]['image'] for idx in idxs]
     # sort according to classes
     incorr = dict(sorted(incorr.items(), key=lambda item: item[0]))
     incorr_disp = {}
     for key, samples in incorr.items():
-        idxs = random.sample(samples, grid_size)
+        idxs = random.sample(samples, min(grid_size, len(samples)))
         incorr_disp[key] = [data[idx]['image'] for idx in idxs]
 
     image_corr = draw_grid(corr_disp, n_classes, grid_size, 800, 600)
@@ -280,10 +272,53 @@ def display(data):
     return
 
 
+def display_dataset_stats(data):
+    """
+    Displays statistics about dataset in a form: class_id: number_of_samples
+    @param data: List of dictionaries, one for every sample, with entry "label" (class_id).
+    @return: Nothing
+    """
+    class_to_num = {}
+    for idx, sample in enumerate(data):
+        class_id = sample['label']
+        if class_id not in class_to_num:
+            class_to_num[class_id] = 0
+        class_to_num[class_id] += 1
+
+    class_to_num = dict(sorted(class_to_num.items(), key=lambda item: item[0]))
+    # print('number of samples for each class:')
+    print(class_to_num)
+
+
+def balance_dataset(data, ratio):
+    """
+    Subsamples dataset according to ratio.
+    @param data: List of samples.
+    @param ratio: Ratio of samples to be returned.
+    @return: Subsampled dataset.
+    """
+    sampled_data = random.sample(data, int(ratio * len(data)))
+
+    return sampled_data
+
+
+
 def main():
     data_train = load_data('./', 'Train.csv')
-    data_test = load_data('./', 'Test.csv')
+    print('train dataset before balancing:')
+    display_dataset_stats(data_train)
+    data_train = balance_dataset(data_train, 1.0)
+    print('train dataset after balancing:')
+    display_dataset_stats(data_train)
 
+    data_test = load_data('./', 'Test.csv')
+    print('test dataset before balancing:')
+    display_dataset_stats(data_test)
+    data_test = balance_dataset(data_test, 1.0)
+    print('test dataset after balancing:')
+    display_dataset_stats(data_test)
+
+    # you can comment those lines after dictionary is learned and saved to disk.
     print('learning BoVW')
     if os.path.isfile('voc.npy'):
         print('BoVW is already learned')
@@ -292,11 +327,14 @@ def main():
 
     print('extracting train features')
     data_train = extract_features(data_train)
+
     print('training')
     rf = train(data_train)
+
     print('extracting test features')
     data_test = extract_features(data_test)
-    print('testing')
+
+    print('testing on testing dataset')
     data_test = predict(rf, data_test)
     evaluate(data_test)
     display(data_test)
@@ -306,3 +344,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
